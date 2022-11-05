ec9d574da59a3e962920cb0e68b381349f01fab8
Federicis Alexandru Ionuț <federicis.alexandru@yahoo.com>
2022-11-05 22:58:53 +0200

    bot cu 2 taskuri

--- backend/redditwrapper/reddit.py
+++ backend/redditwrapper/reddit.py
@@ -9,10 +9,11 @@ import os
 
 
 class Bot:
-    def __init__(self, keywords, replystr, botname, redditor, cusername, cpassword, cclient_id, cclient_secret, subreddit):
+    def __init__(self, botname, redditor, cusername, cpassword, cclient_id, cclient_secret, subreddit = "", replystr = "", welcomestr = "", keywords = []):
         self.r = self.login(botname, redditor, cusername, cpassword, cclient_id, cclient_secret)
         self.keywords = keywords
         self.replystr = replystr
+        self.welcomestr = welcomestr
         self.subreddit = subreddit
 
     def login(self, botname, redditor, cusername, cpassword, cclient_id, cclient_secret):
@@ -26,28 +27,35 @@ class Bot:
 
         return r
 
-    def run(self):
+    def task_reply(self):
         print("Searching last 1,000 comments")
+        if bool(self.keywords) and self.replystr and self.subreddit:
+            for comment in self.r.subreddit(self.subreddit).comments(limit=10000):
+                res = [keyword for keyword in self.keywords if (keyword in comment.body)]
+                if bool(res) and comment.author != self.r.user.me():
+                    comment.refresh()
+                    if self.r.user.me().name not in [x.author.name for x in comment.replies]:
+                        print("my name is " + self.r.user.me().name)
+                        print([x.author.name for x in comment.replies])
+                        comment.reply(self.replystr)
+                        print("Replied to comment " + comment.id)
 
-        for comment in self.r.subreddit(self.subreddit).comments(limit=10000):
-            res = [keyword for keyword in self.keywords if (keyword in comment.body)]
-            if bool(res) and comment.author != self.r.user.me():
-                comment.refresh()
-                if self.r.user.me().name not in [x.author.name for x in comment.replies.list()]:
-                    print("my name is " + self.r.user.me().name)
-                    print(comment.replies.list())
-                    print([x.author.name for x in comment.replies])
-                    comment.reply(self.replystr)
-                    print("Replied to comment " + comment.id)
+            print("Search Completed.")
 
-        print("Search Completed.")
-
-        print("Sleeping for 10 seconds...")
-        # Sleep for 10 seconds...
-        time.sleep(10)
+    def task_firstComment(self):
+        print("Searching last 100 posts...")
+        for submission in self.r.subreddit(self.subreddit).new(limit=100):
+            print("New post!")
+            if self.r.user.me().name not in [x.author.name for x in submission.comments]:
+                print("commenting...")
+                submission.reply(self.welcomestr)
+                print("commented")
 
 
-bot = Bot(["dfs"], "I like your dfs!", "testbot1", "tsaaaaaaaa", "tsaaaaaaaa", "parola123", "23bQR7XTzbODBmXw2WQHlg", "Qm8T8hV2SjHmdQGUYk6gKz5NZbT19w", "tsaa")
+bot = Bot("testbot1", "tsaaaaaaaa", "tsaaaaaaaa", "parola123", "23bQR7XTzbODBmXw2WQHlg", "Qm8T8hV2SjHmdQGUYk6gKz5NZbT19w", subreddit= "tsaa", replystr= "cum sterg",welcomestr= "Tsaa! What a great post!", keywords= ["I like your dfs!"])
 
 while True:
-    bot.run()
+    bot.task_firstComment()
+    bot.task_reply()
+    time.sleep(10)
+    print("Sleeping for 10 seconds....")
